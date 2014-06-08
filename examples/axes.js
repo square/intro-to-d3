648855fa42822c0bd653b60f98bf4afeb4b5700a
Zach Margolis <margolis@squareup.com>
2014-06-07 23:24:25 -0700

    Parts of a graph

---
+++ examples/axes.js
@@ -0,0 +1,14 @@
+var xAxis = d3.svg.axis()
+  .scale(x)         // x is the d3.time.scale()
+  .orient('bottom') // the ticks go below the graph
+  .ticks(4);        // specify the number of ticks
+
+var svg = d3.select('body')
+  .append('svg')        // create an <svg> element
+    .attr('width', 300) // set its dimentions
+    .attr('height', 150);
+
+svg.append('g')            // create a <g> element
+  .attr('class', 'x axis') // specify classes
+  .call(xAxis);            // let the axis to its thing
+
---
+++ examples/data.js
@@ -0,0 +1,7 @@
+var numbers = [ 5, 4, 10, 1 ],
+    data = [
+      { date: '2014-1-1', amount: 10 },
+      { date: '2014-2-1', amount: 20 },
+      { date: '2014-3-1', amount: 40 },
+      { date: '2014-4-1', amount: 80 }
+    ];
