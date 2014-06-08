fdcd471208033ab9ee250e9059483956dfca6297
Zach Margolis <margolis@squareup.com>
2014-06-07 19:06:06 -0700

    Add a demo graph and a table

---
+++ javascripts/parts-of-a-graph.js
@@ -0,0 +1,81 @@
+(function() {
+  /* First graph example, simple line chart */
+  var data = [];
+
+  d3.selectAll('.ex-1 .example-source tbody tr').each(function() {
+    var row = d3.select(this),
+        date = null,
+        amount = null;
+
+    row.selectAll('td').each(function(d, i) {
+      var td = d3.select(this);
+      if (i == 0) {
+        date = new Date(td.text());
+      } else {
+        amount = parseFloat(td.text().replace(/\D/, ''));
+      }
+    })
+
+    data.push({
+      date: date,
+      amount: amount
+    });
+  });
+
+  function getDate(d) {
+    return d.date;
+  }
+
+  function getAmount(d) {
+    return d.amount;
+  }
+
+  function translate(x, y) {
+    return 'translate(' + x + ',' + y + ')';
+  }
+
+  var width = 350,
+      height = 120,
+      margin = { top: 10, right: 50, bottom: 30, left: 50 },
+      x = d3.time.scale()
+          .range([0, width])
+          .domain(d3.extent(data, getDate)),
+      y = d3.scale.linear()
+          .range([height, 0])
+          .domain([0, d3.max(data, getAmount)]),
+      xAxis = d3.svg.axis()
+               .scale(x)
+               .orient('bottom')
+               .ticks(3),
+      yAxis = d3.svg.axis()
+              .scale(y)
+              .orient('left')
+              .tickFormat(d3.format('$,d'))
+              .ticks(5);
+
+  var svg = d3.select('.ex-1 .example-result')
+    .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+      .attr('transform', translate(margin.left, margin.top));
+
+  svg.append('g')
+    .attr('class', 'x axis')
+    .attr('transform', translate(0, height))
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('class', 'y axis')
+    .call(yAxis);
+
+  var line = d3.svg.line()
+      .x(function(d) { return x(getDate(d)) })
+      .y(function(d) { return y(getAmount(d)) })
+
+  svg.append("path")
+    .datum(data)
+    .attr("class", "line")
+    .attr("d", line);
+})();
+
--- z02-parts-of-a-graph.md
+++ z02-parts-of-a-graph.md
@@ -4,6 +4,53 @@ title: Parts of a Graph
 permalink: /parts-of-a-graph/
 ---
 
+The most common visualizations use standard chart formats because they're easy
+understand.
+
+If we had a table in Excel and wanted to graph it, it's easy! Just point
+it at our rows and columns, set a few colors, and boom! A graph.
+
+<div class="ex-1 example-row-2">
+  <div class="example example-source">
+    <table class="data-table">
+      <colgroup>
+        <col class="numeric-col purple-text" />
+        <col class="numeric-col green-text" />
+      </colgroup>
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Amount</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2014-1-1</td>
+          <td>$10</td>
+        </tr>
+        <tr>
+          <td>2014-2-1</td>
+          <td>$20</td>
+        </tr>
+        <tr>
+          <td>2014-3-1</td>
+          <td>$40</td>
+        </tr>
+        <tr>
+          <td>2014-4-1</td>
+          <td>$80</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <div class="example example-result">
+  </div>
+</div>
+
+So now we want to make one of these in SVG so we show it off on the interwebs.
+It's going to be a bit more work.
+
 So what does D3 actually do?
 
 axis / scales / coordinate systems (input data maps to here, x/y maps to screen)
@@ -13,3 +60,5 @@ units / labels (auto-generate date labels, etc)
 Dates!
 colors are just another scale!
 ZOMG color 10
+
+<script type="text/javascript" src="{{ "/javascripts/parts-of-a-graph.js" | prepend: site.baseurl }}"></script>
